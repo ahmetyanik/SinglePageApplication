ce5a94b784d4a41e0655328cd4e262bf1b9a96c2
ahmetyanik <ahmetyank4242@gmail.com>
2021-11-19 21:37:34 +0100

    ready

---
+++ CALISMALAR/2ASYNC-AWAIT/async1/index.js
@@ -0,0 +1,19 @@
+/* 
+
+async function getData(){
+
+   const user = await fetch("https://jsonplaceholder.typicode.com/todos");
+
+   let jsonlar = await user.json();
+
+   let user1 =  jsonlar[0]
+
+   console.log(user1);
+
+   
+}
+
+
+console.log(getData());
+
+ */
