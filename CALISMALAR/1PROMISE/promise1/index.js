0ffb66e28e0192db0531d4959fa1b20604808747
ahmetyanik <ahmetyank4242@gmail.com>
2021-11-21 00:41:56 +0100

    ready

--- CALISMALAR/1PROMISE/promise1/index.js
+++ CALISMALAR/1PROMISE/promise1/index.js
@@ -1,4 +1,4 @@
-
+/* 
 
 function getData() {
 
@@ -16,3 +16,70 @@ function getData() {
 getData()
   .then((data) => console.log("Ahmet"))
   .then((data) => console.log("Murat"));
+
+
+   */
+/* 
+let sayi = 0;
+
+
+function getData(){
+
+   const sonuc = new Promise((resolve, reject) => {
+  
+    if (true) {
+      resolve(5);
+    } else {
+      reject("Olumsuz");
+    }
+  });
+
+  sonuc.then(data=>{
+    sayi = data;
+    console.log(data)});  
+  
+}
+
+getData()
+
+
+setTimeout(()=>{
+
+  console.log(sayi);
+
+},2000)
+
+
+ */
+/* 
+let sayi = 0;
+
+function getData() {
+  const sonuc = new Promise((resolve, reject) => {
+    if (true) {
+      resolve(5);
+    } else {
+      reject("Olumsuz");
+    }
+  });
+
+  sonuc.then((data) => {
+    sayi = data;
+    console.log(sayi);
+  });
+}
+
+
+
+
+async function siraliSonuc() {
+  await getData();
+  console.log(sayi + 60);
+}
+
+siraliSonuc();
+ */
+
+
+
+
