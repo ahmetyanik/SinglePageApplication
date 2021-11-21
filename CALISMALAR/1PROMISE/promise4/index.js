083acefa957120f62312b6d654fa2145d58fb095
ahmetyanik <ahmetyank4242@gmail.com>
2021-11-21 18:13:07 +0100

    ready

---
+++ CALISMALAR/1PROMISE/promise4/index.js
@@ -0,0 +1,47 @@
+const array = [];
+
+function kisiEkle() {
+  const input = prompt("Kisi giriniz:");
+
+  const istek = new Promise((resolve, reject) => {
+    if (!input || input.length === 0) {
+      reject("Herhangi bir kisi eklemediniz!");
+    } else if (input === "q") {
+      console.log("********Kisiler**********");
+
+      array.forEach((kisi) => {
+        console.log(kisi);
+      });
+
+      reject(sonuc());
+    } else if (input.length > 0) {
+      array.push(input);
+
+      resolve(`${input} adli kisi eklendi...`);
+    }
+  });
+
+  return istek;
+}
+
+function sonuc() {
+  const randomSayi = Math.floor(Math.random() * array.length - 1);
+
+  console.log("**********YEMEGI ÖDEYEN**********");
+
+  console.log(array[randomSayi]);
+
+}
+
+function oyna() {
+  function tekrar() {
+    kisiEkle()
+      .then((data) => console.log(data))
+      .then(tekrar)
+      .catch(err=>err);
+  }
+
+  tekrar();
+}
+
+oyna();
