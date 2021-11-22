d86a1f8b3bf083667979f22b10dbb68349254003
ahmetyanik <ahmetyank4242@gmail.com>
2021-11-22 23:21:33 +0100

    Not ready

--- MemoryCards/index.js
+++ MemoryCards/index.js
@@ -1,111 +1,117 @@
+let acikElemanlar = 0;
+let ikiElement = ["deneme", "deneme"];
 const main = document.querySelector("main");
-let animals;
-let openAnimalNumber = 0;
-let openedArray = [];
+const divler = document.querySelectorAll("main>div");
+const button = document.querySelector("button");
+const hamle = document.querySelector("#hamle");
+let gonderilmisMain;
+let hamleSayisi = 0;
 
-async function startGame() {
-  let randomNumber;
-  
-  const animals = await fetch("animal.json");
-  const animalsJSON = await animals.json();
-  
-  return animalsJSON;  
-  
+async function verileriAl() {
+  const tumHayvanlar = await fetch("animal.json");
+  const tumHayvanlarJSON = await tumHayvanlar.json();
+
+  return tumHayvanlarJSON;
 }
 
+async function hayvanlariMaineGonder() {
+  main.innerText = "";
 
+  const tumHayvanlar = await verileriAl();
 
-function addToMain(){
-
-  animalNumber = animals.length;
-
-  animals.forEach((animal) => {
-    randomNumber = Math.floor(Math.random() * animalNumber);
-    
+  tumHayvanlar.forEach((hayvan) => {
     const div = document.createElement("div");
-    div.style.backgroundImage = animal.image;
+    div.style.backgroundImage = hayvan.image;
     div.style.display = "flex";
-    div.style.order = randomNumber;
+    div.style.order = Math.floor(Math.random() * tumHayvanlar.length);
+    div.style.backgroundImage = hayvan.closedImage;
+    div.setAttribute("name", hayvan.name);
+    div.setAttribute("isopen", hayvan.isOpen);
+    div.setAttribute("image", hayvan.image);
+    div.setAttribute("closedImage", hayvan.closedImage);
+    div.setAttribute("matched", hayvan.matched);
+
     main.appendChild(div);
-    animalNumber--;
-
-  
-    // if isOpen of animals are false, give them cover background 
-  
-    if (animal.isOpen === false) {
-      div.style.backgroundImage =
-        "url('https://teknosafari.net/wp-content/uploads/2020/10/ucretsiz-en-iyi-wallpaper-siteleri-12.jpg')";
-    } 
-  
-  
-    // Wenn you click a div :
-  
-    div.addEventListener("click", (e) => {
-      e.stopPropagation();
-  
-  
-      animal.isOpen = true;     // Give the new value for isOpen
-      openAnimalNumber++;       // Add number of animal +1 
-  
-
-
-      openedArray.push(animal);
-
-      console.log(animals);
-
-      console.log(openedArray);
-
-
-      if(openedArray.length===3){
-
-        console.log("hello");
-  
-        animals.forEach(animal=>{
-  
-          if(animal.name === openedArray[0] || animal.name === openedArray[1]){
-  
-            animal.isOpen = true;
-          }else{
-  
-            animal.isOpen = false;
-          }
-        })
-  
-        openedArray = [];
-        console.log(openedArray);
-  
-    }
-
- 
-  
-    }); 
-    
-    
-   
-
-
-
-
-
-
-  
   });
 
-
-  console.log(animals);
-
+  return main;
 }
 
+button.addEventListener("click", () => {
+  hamleSayisi = 0;
+  hayvanlariMaineGonder();
 
+  console.log(main);
 
+  button.innerText = "YENI OYUN";
 
+});
 
+console.log(ikiElement);
 
-startGame()
-.then(data=>animals = data)
-.then(addToMain)
+main.addEventListener("click", (e) => {
+  console.log(ikiElement);
+  let element = e.target;
+  const divler = document.querySelectorAll("main>div");
+  console.log(divler);
 
+  if (acikElemanlar < 2) {
+    if (element.getAttribute("isopen") === "false") {
+      element.setAttribute("isopen", "true");
+      element.style.backgroundImage = element.getAttribute("image");
+      acikElemanlar++;
+      hamleSayisi++;
+      console.log(acikElemanlar);
+      console.log(element);
 
+      ikiElement.push(element);
+      ikiElement.shift(element);
 
+      kontrol();
+    }
+  } else {
+    divler.forEach((div) => {
+      div.setAttribute("isopen", "false");
+      div.style.backgroundImage = div.getAttribute("closedImage");
+      acikElemanlar = 0;
+    });
+  }
 
+  hamle.innerText = `${hamleSayisi}`;
+  hamle.style.fontSize = "5rem";
+});
 
+async function kontrol() {
+  console.log(main);
+
+  console.log("kontrolde");
+
+  if (acikElemanlar >= 2) {
+    console.log(ikiElement);
+
+    console.log("iki eleman acildi");
+    console.log(ikiElement);
+
+    console.log(ikiElement[0].getAttribute("name"));
+    console.log(ikiElement[1].getAttribute("matched"));
+
+    if (
+      ikiElement[0].getAttribute("name") ===
+      ikiElement[1].getAttribute("matched")
+    ) {
+      document.querySelectorAll("main>div").forEach((div) => {
+        console.log(div.getAttribute("name"));
+
+        if (
+          div.getAttribute("name") === ikiElement[0].getAttribute("name") ||
+          div.getAttribute("name") === ikiElement[1].getAttribute("name")
+        ) {
+          console.log(div);
+
+          div.style.backgroundImage = div.getAttribute("image");
+          div.setAttribute("closedImage", div.getAttribute("image"));
+        }
+      });
+    }
+  }
+}
