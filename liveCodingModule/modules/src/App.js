fc4979fa933c6a8394d624327ada0d6762461976
ahmetyanik <ahmetyank4242@gmail.com>
2021-11-30 21:16:21 +0100

    ready

---
+++ liveCodingModule/modules/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import FancyButton from './FancyButton';
+import Header from './Header';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <FancyButton/>
+     <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ liveCodingModule/modules/src/FancyButton.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+function FancyButton(){
+    return(
+  
+      <button className="FancyButton">
+        <span className="FancyButton-label">Click Me!</span>
+        <span className="FancyButton-spinner"></span>
+  
+      </button>
+    )
+}
+
+export default FancyButton;
