44c7c73efd037974cc3a294f89351e39f967a407
ahmetyanik <ahmetyank4242@gmail.com>
2021-12-06 23:12:06 +0100

    ready

--- livecodinform/src/App.js
+++ livecodinform/src/App.js
@@ -4,20 +4,45 @@ import {useState} from "react";
 
 function App() {
 
-  const [name,setName] = useState("");
+  const [name,setName] = useState({
+    isim:"",
+    soyisim : ""
+  });
+
+/* 
 
   const inputChange = (e)  =>{
 
-    const val = e.target.value;
-    setName(val);
+    const val = e.target.name;
+    setName([val]);
+
   }
+
+   */
+
+   const ekle = (e) =>{
+
+      const gelenVeri = e.target.value;
+
+      console.log(gelenVeri);
+
+      setName((previousDatas)=>{
+
+        return{
+          ...previousDatas,
+          [e.target.name]:gelenVeri
+        }
+
+      })
+   }
 
   return (
     <div className="App">
       
-      <h1>Hallo {name}</h1>
+      <h1>Hallo {name.isim} {name.soyisim} </h1>
 
-      <input onChange={inputChange} type="text"/>
+      <input name = "isim"  onChange={ekle} type="text"/>
+      <input name = "soyisim"  onChange={ekle} type="text"/>
 
     </div>
   );
