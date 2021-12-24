99ab232fe9bc392ec48c0fa73cba66f5165fc7bf
ahmetyanik <ahmetyank4242@gmail.com>
2021-12-24 01:16:36 +0100

    v1

---
+++ my-project/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl bg-primary font-bold underline">
+      Hello world!
+      <br></br>
+      <span>Ahmet</span><span className='mx-5'>Yanik</span>
+
+      <article class="prose lg:prose-xl">
+  <h1>Garlic bread with cheese: What the science tells us</h1>
+  <p>
+    For years parents have espoused the health benefits of eating garlic bread with cheese to their
+    children, with the food earning such an iconic status in our culture that kids will often dress
+    up as warm, cheesy loaf for Halloween.
+  </p>
+  <p>
+    But a recent study shows that the celebrated appetizer may be linked to a series of rabies cases
+    springing up around the country.
+  </p>
+</article>
+
+
+<select class="px-4 py-3 rounded-full">
+ 
+</select>
+
+<input type="checkbox" class="rounded text-pink-500" />
+    </h1>
+  );
+}
+
+export default App;
---
+++ my-project/tailwind.config.js
@@ -0,0 +1,66 @@
+const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },    
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#evebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      "primary" : '#5c6ac4',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      brown: {
+        50: '#fdf8f6',
+        100: '#f2e8e5',
+        200: '#eaddd7',
+        300: '#e0cec7',
+        400: '#d2bab0',
+        500: '#bfa094',
+        600: '#a18072',
+        700: '#977669',
+        800: '#846358',
+        900: '#43302b',
+      },
+      
+    },
+  },
+  plugins: [
+    
+    plugin(function({ addUtilities, addComponents, e, prefix, config }) {
+      // Add your custom styles here
+    }),
+  ]
+}
