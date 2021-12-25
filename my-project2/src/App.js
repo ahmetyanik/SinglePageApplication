f4b13de4dbd7294c1d24e8e88aec8fd99a9cc71e
ahmetyanik <ahmetyank4242@gmail.com>
2021-12-25 02:21:16 +0100

    tailwind

--- my-project2/src/App.js
+++ my-project2/src/App.js
@@ -1,66 +1,27 @@
 import "./App.css";
 
 function App() {
-
-  const puan = [1,1,1,1];
-
- 
-
   return (
-    <div class="max-w-2xl mx-auto">
+    <div class="flex justify-center mt-2">
+      <div
+        style={{ height: "500px" }}
+        className="border box-border  shadow-md shadow-teal-700 "
+      >
+        <img
+          className="object-cover w-96 h-96"
+          src="https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1024&h=1280&q=80"
+        />
 
-
-    <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-      <a href="#">
-        <img class="rounded-t-lg p-8" src="https://flowbite.com/docs/images/products/product-1.png" alt="product image"/>
-          </a>
-        <div class="px-5 pb-5">
-          <a href="#">
-            <h3 class="text-gray-900 font-semibold text-xl tracking-tight dark:text-white">Apple Watch Series 7
-              GPS, Aluminium Case, Starlight Sport</h3>
-          </a>
-          <div class="flex items-center mt-2.5 mb-5">
-
-            {
-
-               puan.forEach(element=>{
-
-               return(
-
-              <svg class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
-              </path>
-            </svg> 
-
-               ) 
-
-
-              })      
-              
-
-            }
-         
-            
-            <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
-          </div>
-          <div class="flex items-center justify-between">
-            <span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-            <a href="#"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add
-              to cart</a>
-          </div>
+        <div className="text-center">
+          <label className="animate-bounce w-6 h-6">Isim:</label>
+          <span>Ahmet</span>
         </div>
+      </div>
+
+      <textarea class="caret-indigo-500"></textarea>
+
+     
     </div>
-  
-  
-    <p class="mt-5">This card component is part of a larger, open-source library of Tailwind CSS components. Learn
-      more
-      by going to the official <a class="text-blue-600 hover:underline"
-        href="https://flowbite.com/docs/getting-started/introduction/" target="_blank">Flowbite Documentation</a>.
-    </p>
-  </div>
   );
 }
 
